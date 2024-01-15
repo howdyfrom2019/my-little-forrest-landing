627cb734fa799da83cdfec4a3c66b2cd9c799d1f
howdyfrom2019 <tjd1emd@naver.com>
2024-01-15 23:32:36 +0900

    Add tailwind color config

--- src/lib/utils/tailwindUtil.ts
+++ src/lib/utils/tailwindUtil.ts
@@ -4,3 +4,69 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const breakpoints = {
+  xs: 280,
+  sm: 640,
+  md: 768,
+  lg: 1200,
+  xl: 1470,
+  '2xl': 1740,
+};
+
+export const colors = {
+  white: {
+    500: '#f1f5f9',
+    900: '#fff',
+    DEFAULT: '#fff',
+    transparent: {
+      50: '#ffffff80',
+    },
+  },
+  black: {
+    400: '#404040',
+    500: '#1e1e1e',
+    900: '#000000',
+    DEFAULT: '#000000',
+    transparent: {
+      30: '#00000048',
+      40: '#00000064',
+    },
+  },
+  green: {
+    300: '#c8edac',
+    500: '#219025',
+    DEFAULT: '#219025',
+  },
+  blue: {
+    300: '#7491be',
+    500: '#0069ff',
+    DEFAULT: '#0069ff',
+  },
+  red: {
+    300: '#e77b7c',
+    500: '#c42e30',
+    DEFAULT: '#c42e30',
+  },
+  mint: {
+    300: '#6ba9bo',
+    500: '#15c7db',
+    DEFAULT: '#15c7db',
+  },
+};
+
+export const typos = {
+  '.typo-2xl': {
+    fontFamily: 'var(--font-tenada)',
+    fontSize: '24px',
+    letterSpacing: '0.24px',
+    fontStyle: 'normal',
+  },
+  '.typo-4xl': {
+    fontFamily: 'var(--font-ibm-plex-sans)',
+    fontSize: '64px',
+    letterSpacing: '-1.28px',
+    fontWeight: '700',
+    fontStyle: 'normal',
+  },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
+import { breakpoints, colors, typos } from '@/lib/utils/tailwindUtil';
 
 const config: Config = {
   darkMode: 'class',
@@ -8,18 +10,38 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    fontFamily: {
-      sans: ['var(--font-ibm-plex-sans)'],
-      tenada: ['var(--font-tenada)'],
-    },
     extend: {
+      colors: colors,
+      fontFamily: {
+        sans: ['var(--font-ibm-plex-sans)'],
+        tenada: ['var(--font-tenada)'],
+      },
+      screens: {
+        xs: `${breakpoints.xs}px`,
+        sm: `${breakpoints.sm}px`,
+        md: `${breakpoints.md}px`,
+        lg: `${breakpoints.lg}px`,
+        xl: `${breakpoints.xl}px`,
+        '2xl': `${breakpoints['2xl']}px`,
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      zIndex: {
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+      },
     },
   },
-  plugins: [],
+  plugins: [plugin(({ addUtilities }) => addUtilities(typos))],
 };
 export default config;
