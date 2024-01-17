078e15eee18d6401477c79f99082e9ba4399eef3
howdyfrom2019 <tjd1emd@naver.com>
2024-01-18 01:56:39 +0900

    Fix trapezoid

--- src/components/TrapeZoid.tsx
+++ src/components/TrapeZoid.tsx
@@ -10,40 +10,15 @@ const TrapeZoid = ({
 }: ComponentPropsWithoutRef<'svg'>) => {
   return (
     <svg
-      width={width ?? '754'}
-      height={height ?? '863'}
-      viewBox={viewBox ?? '0 0 754 863'}
+      width={width ?? '746'}
+      height={height ?? '855'}
+      viewBox={viewBox ?? '0 0 746 855'}
       fill={fill ?? 'current'}
       xmlns="http://www.w3.org/2000/svg"
       className={className}
       {...props}
     >
-      <g filter="url(#filter0_f_95_6)">
-        <path d="M129.5 4H749.69V858.5H4L129.5 4Z" fill="current" />
-      </g>
-      <defs>
-        <filter
-          id="filter0_f_95_6"
-          x="0"
-          y="0"
-          width="753.69"
-          height="862.5"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
-        >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="BackgroundImageFix"
-            result="shape"
-          />
-          <feGaussianBlur
-            stdDeviation="2"
-            result="effect1_foregroundBlur_95_6"
-          />
-        </filter>
-      </defs>
+      <path d="M125.5 0H745.69V854.5H0L125.5 0Z" fill="current" />
     </svg>
   );
 };
